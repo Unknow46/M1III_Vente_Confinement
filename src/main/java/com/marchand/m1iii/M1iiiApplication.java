fe770ebb2e3a93804180c438b5724ce7ec595072
Nathan <nathan.sens@hotmail.fr>
2020-03-18 08:30:48 +0100

    Adding project

---
+++ src/main/java/com/marchand/m1iii/M1iiiApplication.java
@@ -0,0 +1,13 @@
+package com.marchand.m1iii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1iiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1iiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marchand/m1iii/M1iiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchand.m1iii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1iiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
