b30f33f3f7858cec4ea83fcc70e1dcde9da80252
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-03-18 15:56:16 +0100

    :card_file_box: up the data sql file bis because i didnt but foreign key

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-
-INSERT INTO Commande(Id, date_commande, prix_total, montant_frais) VALUES (1, '17/03/2020', 42, 12);
-INSERT INTO Commande(Id, date_commande, prix_total, montant_frais) VALUES (2, '13/03/2020', 36, 12);
-INSERT INTO Commande(Id, date_commande, prix_total, montant_frais) VALUES (3, '16/03/2020', 12, 5);
+// j'ai un doute pour la valeur Ligne dans la table Commande
+INSERT INTO Commande(Id, date_commande, prix_total, montant_frais, ligne, personne, adresse) VALUES (1, '17/03/2020', 42, 12, 1, 3, 1);
+INSERT INTO Commande(Id, date_commande, prix_total, montant_frais, ligne, personne, adresse) VALUES (2, '13/03/2020', 36, 12, 2, 2, 3);
+INSERT INTO Commande(Id, date_commande, prix_total, montant_frais, ligne, personne, adresse) VALUES (3, '16/03/2020', 12, 5, 3, 1, 2);
 
 INSERT INTO Personne(Id, nom, prenom, date_naissance) VALUES (1, 'toto', 'titi', '02/03/1994');
 INSERT INTO Personne(Id, nom, prenom, date_naissance) VALUES (2, 'Tutu', 'Tyty', '02/05/1986');
@@ -11,17 +11,18 @@ INSERT INTO Rayon(Id, libelle) VALUES (1, 'vetements');
 INSERT INTO Rayon(Id, libelle) VALUES (2, 'nourriture');
 INSERT INTO Rayon(Id, libelle) VALUES (3, 'jeux-video');
 
-INSERT INTO Modele(Id, libelle, taille) VALUES (1, 'pantalon', 'xl');
-INSERT INTO Modele(Id, libelle, taille) VALUES (2, 't-shirt', 'xl');
-INSERT INTO Modele(Id, libelle, taille) VALUES (3, 'chemise', 'xl');
+INSERT INTO Modele(Id, libelle, taille, rayon) VALUES (1, 'pantalon', 'xl', 1);
+INSERT INTO Modele(Id, libelle, taille, rayon) VALUES (2, 't-shirt', 'xl', 1);
+INSERT INTO Modele(Id, libelle, taille, rayon) VALUES (3, 'chemise', 'xl', 1);
 
-INSERT INTO Ligne(Id, quantite, prix_facture_unitaire) VALUES (1, 36, 420);
-INSERT INTO Ligne(Id, quantite, prix_facture_unitaire) VALUES (2, 36, 420);
-INSERT INTO Ligne(Id, quantite, prix_facture_unitaire) VALUES (3, 36, 420);
+INSERT INTO Ligne(Id, quantite, prix_facture_unitaire, article) VALUES (1, 36, 420, 1);
+INSERT INTO Ligne(Id, quantite, prix_facture_unitaire, article) VALUES (2, 36, 420, 2);
+INSERT INTO Ligne(Id, quantite, prix_facture_unitaire, article) VALUES (3, 36, 420, 3);
 
-INSERT INTO Article(Id, libelle, couleur, prix_unitaire) VALUES (1, 'pantalon', 'vert', 34);
-INSERT INTO Article(Id, libelle, couleur, prix_unitaire) VALUES (2, 'pantalon', 'rouge', 34);
-INSERT INTO Article(Id, libelle, couleur, prix_unitaire) VALUES (3, 'pantalon', 'jeune', 34);
+INSERT INTO Article(Id, libelle, couleur, prix_unitaire, modele) VALUES (1, 'pantalon', 'vert', 34, 1);
+INSERT INTO Article(Id, libelle, couleur, prix_unitaire, modele) VALUES (2, 'pantalon', 'rouge', 34, 1);
+INSERT INTO Article(Id, libelle, couleur, prix_unitaire, modele) VALUES (3, 'chemise', 'jaune', 34, 3);
 
 INSERT INTO Adresse(Id, ligne_1, ligne_2, cp, ville, pays) VALUES (1, '41 rue du port', 'rizom', '59000', 'Lille', 'France');
-INSERT INTO Adresse(Id, ligne_1, ligne_2, cp, ville, pays) VALUES (1, '60 Boulevard Vauban', 'hotal academique', '59800', 'Lille', 'France');
+INSERT INTO Adresse(Id, ligne_1, ligne_2, cp, ville, pays) VALUES (2, '60 Boulevard Vauban', 'hotal academique', '59800', 'Lille', 'France');
+INSERT INTO Adresse(Id, ligne_1, ligne_2, cp, ville, pays) VALUES (3, '2 rue d Isly', ' ', '59000', 'Lille', 'France');
