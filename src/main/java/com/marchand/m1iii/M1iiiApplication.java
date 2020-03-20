4d74235064f62b083b817af06fc303418a208f4a
croquelois <croquelois.adrien@gmail.com>
2020-03-20 19:36:52 +0100

    :sparkles: add swagger

--- src/main/java/com/marchand/m1iii/M1iiiApplication.java
+++ src/main/java/com/marchand/m1iii/M1iiiApplication.java
@@ -1,13 +1,40 @@
 package com.marchand.m1iii;
 
+import com.marchand.m1iii.controller.ArticleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@ComponentScan(basePackageClasses = {
+		ArticleController.class
+})
 public class M1iiiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(M1iiiApplication.class, args);
 	}
 
+	@Bean
+	public Docket petApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class);
+	}
 }
